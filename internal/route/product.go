00a8075c7c1cd23940eb1ca4fab91a897a5a73a5
fadhilaf <falfatih3421@gmail.com>
2026-05-22 23:24:31 +0700

    fix: update authentication for creating product and seeing supplier

--- internal/route/product.go
+++ internal/route/product.go
@@ -2,16 +2,18 @@ package route
 
 import (
 	delivery "github.com/fadhilaf/s-tech/internal/delivery/product"
+	"github.com/fadhilaf/s-tech/internal/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 func ProductRoutes(router *gin.RouterGroup, handler delivery.ProductDelivery) {
-	router.POST("/", handler.CreateProduct)
-	router.POST("/supplier", handler.CreateSupplier)
-	router.POST("/receive_stock", handler.ReceiveProductStock)
-	
 	router.GET("/", handler.GetProducts)
-	router.GET("/supplier", handler.GetSuppliers)
 	router.GET("/:id", handler.GetProductById)
+
+	adminGroup := router.Group("/admin", middleware.ShouldAdmin())
+	adminGroup.GET("/supplier", handler.GetSuppliers)
+	adminGroup.POST("/", handler.CreateProduct)
+	adminGroup.POST("/supplier", handler.CreateSupplier)
+	adminGroup.POST("/receive_stock", handler.ReceiveProductStock)
 }
